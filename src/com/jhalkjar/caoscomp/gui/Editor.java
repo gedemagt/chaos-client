2ce9f358b38574397c47076b5adcec11881cc7fc
Jesper Halkjær Jensen <gedemagt@gmail.com>
2018-02-07 17:27:35 +0100

    Added some error handling and another way for versioning.

--- src/com/jhalkjar/caoscomp/gui/Editor.java
+++ src/com/jhalkjar/caoscomp/gui/Editor.java
@@ -54,7 +54,6 @@ public class Editor extends Form {
         canvas = new Canvas();
         axis = new Axis(canvas);
 
-
         populateToolbar(edit);
         add(BorderLayout.NORTH, l);
         if(edit) {
